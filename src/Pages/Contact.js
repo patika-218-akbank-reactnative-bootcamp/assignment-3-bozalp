1b4c95a82886de77ecf3a282f5e4b72d5538ef59
Batuhan ÖZALP <batuhan.ozalp.9@yandex.com>
2022-09-02 17:43:53 +0300

    contact list added

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,14 +1,25 @@
 import React, { useContext } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, FlatList, TouchableOpacity, Image } from 'react-native';
 import ThemeContext from '../Context/ThemeContext';
+
+import ContactList from '../JsonFiles/ContactList.json';
 
 const Contact = () => {
     const { theme, setTheme } = useContext(ThemeContext);
 
     return (
         <View style={[styles.container, { backgroundColor: theme.backgroundColor }]}>
-            <Text style={[styles.header, { color: theme.color }]}>Contact</Text>
-            <Text style={{ color: theme.color, paddingTop: 10 }}>Burada kişiler sıralanacak</Text>
+            <Text style={[styles.header, { color: theme.color, paddingBottom: 30 }]}>Contact</Text>
+            <FlatList
+                data={ContactList}
+                renderItem={({ item }) =>
+                    <TouchableOpacity activeOpacity={0.7} style={styles.button_area}>
+                        <Image source={{ uri: item.imageUrl }} style={styles.avatar} />
+                        <TouchableOpacity activeOpacity={0.7} style={styles.button}>
+                            <Text style={{ color: theme.color }}>{item.userName}</Text>
+                        </TouchableOpacity>
+                    </TouchableOpacity>
+                } />
         </View>
     );
 }
@@ -25,6 +36,33 @@ const styles = StyleSheet.create(
             fontSize: 18,
             textAlign: 'center',
             fontWeight: 'bold',
+        },
+        button:
+        {
+            width: '100%',
+            height: 72,
+            justifyContent: 'center',
+            textAlignVertical:'center',
+            paddingBottom:10
+        },
+        button_area:
+        {
+            flex: 1,
+            flexDirection: 'row',
+            marginBottom:10,
+        },
+        avatar:
+        {
+            width: 64,
+            height: 64,
+            borderRadius: 32,
+            marginRight: 10
+        },
+        seperator:
+        {
+            width: '100%',
+            borderBottomColor: 1,
+            borderColor: '#ccc',
         }
     }
 );
